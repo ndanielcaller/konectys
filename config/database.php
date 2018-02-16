47839fff9c18099dde7a103c996a730d2a5018e0
Daniel Caller <ndanielcaller@gmail.com>
2018-02-15 21:58:34 -0500

    fix mysql user

--- config/database.php
+++ config/database.php
@@ -54,7 +54,7 @@ return [
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => 'konectys_t',
-            'username' => env('MYSQL_USERNAME',env('DB_USERNAME', 'forge')),
+            'username' => env('MYSQL_USER',env('DB_USERNAME', 'forge')),
             'password' => env('MYSQL_PASSWORD',env('DB_PASSWORD', '')),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8',
